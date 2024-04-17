842768da2307471dd2634064e7f68a5afaa827ef
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-04-17 03:36:34 -0300

    Bank Service/Database prints refixed

--- src/components/bankService/BankService.java
+++ src/components/bankService/BankService.java
@@ -46,7 +46,7 @@ public class BankService extends BaseBankService {
     DTO dtoToRedirect
   ) throws Exception {
     sendSecureDTO(SocketComponent.BANK_DATABASE, dtoToRedirect);
-    Thread.sleep(10);
+    Thread.sleep(100);
     return receiveSecureDTO(SocketComponent.BANK_DATABASE);
   }
 
